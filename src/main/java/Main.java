f3701deac0c047bcfb00f3a7049ff0a8ee78db9e
Flavia <flavia.brogle@hotmail.com>
2020-08-30 12:04:46 +0200

    Improve Creation of a NN
    It's now easier to create a NN of different sizes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,21 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        Network network = new Network(4, 3, 2, 3);
-        double[] output = network.calculate(0.2, 0.3, 0.4, 0.5);
-        for (int i = 0; i < 3; i++) {
-            System.out.println(output[i]);
+        int inputSize = 700;
+        int outputSize = 700;
+        Network network = new Network(inputSize, 100, 100, outputSize);
 
+        double[] inputArray = new double[inputSize];
+        Random r = new Random();
+        for (int i = 0; i < inputSize; i++) {
+            inputArray[i] = r.nextDouble();
         }
+
+        double[] output = network.calculate(inputArray);
+        System.out.println(Arrays.toString(output));
     }
 
 }
